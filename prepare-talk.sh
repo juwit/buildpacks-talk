9c239d1ffbb517e61438dd5b84ed40a161a36f9e
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-09-29 15:40:54 +0200

    ✨ : update slides

--- prepare-talk.sh
+++ prepare-talk.sh
@@ -9,9 +9,9 @@ docker image pull paketobuildpacks/builder:tiny
 docker image pull heroku/builder:22
 docker image pull gcr.io/buildpacks/builder:google-22
 
-docker image pull eclipse-temurin:17.0.7_7-jdk
+docker image pull eclipse-temurin:17.0.8.1_1-jdk
 
 # building petclinic with dockerfile
-cd /home/jwittouck/workspaces/sunny-tech/buildpacks-talk/spring-petclinic/
+cd /home/jwittouck/workspaces/github/buildpacks-talk/spring-petclinic/
 docker image build -t petclinic:dockerfile .
 
